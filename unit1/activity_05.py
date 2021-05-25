afd43bee7796b2ad08054f607796e21b8e4b423e
AdriRiv <dani2.martinez50@gmail.com>
2021-05-25 15:44:49 -0500

    activity_05_finished

---
+++ unit1/activity_05.py
@@ -0,0 +1,12 @@
+
+
+print("Write two numbers to be divided")
+
+num1= int(input("Enter first number: "))
+num2= int(input("Enter second number: "))
+
+div= num1/num2
+print("The result of the division is: ", div)
+
+
+
