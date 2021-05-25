e58fcbb7ae93373ddeb3b54acd3e262c2c84ac4f
AdriRiv <dani2.martinez50@gmail.com>
2021-05-25 03:46:15 -0500

    activity_04_finished

---
+++ unit1/activity_04.py
@@ -0,0 +1,15 @@
+###DECLARE VARIABLES ####
+
+num1= 0
+num2= 0
+sum= 0
+mult= 0
+
+num1= int(input("Enter number 1: "))
+num2= int(input("Enter number 2: "))
+
+sum= num1 + num2
+mult= num1 *num2 
+
+print ("The sum of the numbers is: ", sum)
+print ("The product of the numbers is: ", mult)
