5f57793e1388a8298ca79fe496a114ea561ddb3f
AdriRiv <dani2.martinez50@gmail.com>
2021-05-27 13:37:47 -0500

    quiz_question6_code

---
+++ unit1/final quiz/instruction_06.py
@@ -0,0 +1,17 @@
+
+
+b= 0
+h= 0
+area= 0
+
+b= int(input("Write the length of the rectangle: "))
+h= int(input("Write the width of the rectangle: "))
+
+if (b == h):
+    print("It is not a rectangle, it is a square")
+
+area= b*h
+
+print ("The total area of the figure is: ", area)
+
+
