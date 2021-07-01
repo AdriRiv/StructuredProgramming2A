eec7e4f9bb2442116cc99b1a1133d97d5322278a
AdriRiv <dani2.martinez50@gmail.com>
2021-07-01 11:36:55 -0500

    Bucles while y For python

--- unit2/portafolio/bucles.py
+++ unit2/portafolio/bucles.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     print (mlist, len(mlist)) #Size of the list 
 
     index = 0
+    print("Bucle While")
     while (index<len(mlist) ):
         print ( f'index= {index}, value= {mlist[index]}')
         index = index +1
+    print("Bucle FOR")
     for index in range(0, len(mlist)):
         print ( f'index= {index}, value= {mlist[index]}')
