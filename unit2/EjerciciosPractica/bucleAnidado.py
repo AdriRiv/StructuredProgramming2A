e15ecdaa0e666324855f7a121ecee3b6cf0ccd9b
AdriRiv <dani2.martinez50@gmail.com>
2021-07-02 00:54:02 -0500

    Bucle Anidado Python Done

---
+++ unit2/EjerciciosPractica/bucleAnidado.py
@@ -0,0 +1,15 @@
+
+mylist= [10, 30, 40, 50, 60, 70, 80]
+index = 0
+
+
+if __name__ == "__main__":
+    for index in range(0, len(mylist)):
+        print(f'Index: {index}, Value: {mylist[index]}')
+        for index in range(0, 3):
+            print("Hola Mundo")
+
+
+            
+
+
