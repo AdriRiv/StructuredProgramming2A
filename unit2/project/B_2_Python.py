c7dd341c99cee4e0f8d2d9f627cbd1a450d082a3
AdriRiv <dani2.martinez50@gmail.com>
2021-07-29 01:21:22 -0500

    Section B, question 2, done

--- unit2/project/B_2_Python.py
+++ unit2/project/B_2_Python.py
@@ -1,15 +1,17 @@
+
+
 import sys 
 
-num=int(input("Please enter a number to see if it is divisi ble by 3 and 5"))
+
 
 indx= 0 
 
 def divisible3and5(indx):
-    if(indx%3 & indx%5 == 0 ):
-        print(f'This number is divisible by 3 and 5')   
-
-
+    for indx in range(0, 500):
+        if(indx%3 ==0 ):
+            if (indx%5 == 0 ):
+                print(f'This number is divisible by 3 and 5: ', indx)   
 
 if __name__ == "__main__":
     
-    divisible3and5(num)
+    divisible3and5(indx)
