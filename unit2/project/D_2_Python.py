2ea0256d13f042ee6913534a90cf0f29c0f71e73
AdriRiv <dani2.martinez50@gmail.com>
2021-07-29 20:06:36 -0500

    Section D, question 2, Sucession Python done

---
+++ unit2/project/D_2_Python.py
@@ -0,0 +1,20 @@
+
+
+import sys
+
+list= []
+
+if __name__ == "__main__":
+    for i in range(2,6):
+        list.append(i)
+    for i in range(3,7):
+        list.append(i)
+    for i in range(4,8):
+        list.append(i)
+    for i in range(5,9):
+        list.append(i)
+    
+    print("The succesion is: ", list)
+
+
+
