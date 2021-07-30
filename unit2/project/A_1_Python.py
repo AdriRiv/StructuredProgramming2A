4f1a5cd751769e086cd9916f001390c170c747d9
AdriRiv <dani2.martinez50@gmail.com>
2021-07-29 21:11:28 -0500

    Section A, question A, Python Done

--- unit2/project/A_1_Python.py
+++ unit2/project/A_1_Python.py
@@ -1,20 +1,21 @@
+
+
 import sys
 
 indx=1
 avrg=0
 
 def average(argv):
-    for indx in range(indx <= len(sys.argv) ):
-        
+    avrg=0
+    for indx in range(1, len(sys.argv) ):
         num1= int(sys.argv[indx])
-        numid= num1
-        indx= indx+1 
         avrg= num1+avrg
-        print(num1, numid, indx, avrg)
+        print(num1,avrg)
+    avrg= avrg/(len(sys.argv)-1)
+    print("The average of the numbers is: ", avrg)
        
 
-        avrg= avrg/indx
 
 if __name__ == "__main__":
-    
-    print(f'The average of the numbers you inserted is: {avrg}')
+    print("We are calculating the average, please wait\n")
+    average(sys.argv)
